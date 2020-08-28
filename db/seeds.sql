c632b1dfc1c2ace359319198a59d3844850e469d
Cristhal Fuertes <cristhal.fuertes@gmail.com>
2020-08-28 06:47:11 -0400

    fixed spelling error

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
 INSERT INTO bugers (burger_name, devoured) VALUES ('Cheeseburger', FALSE);
 INSERT INTO bugers (burger_name, devoured) VALUES ('Veggie Burger', FALSE);
-INSERT INTO bugers (burger_name, devoured) VALUES ('Beyond Burger', True);
+INSERT INTO bugers (burger_name, devoured) VALUES ('Beyond Burger', TRUE);
