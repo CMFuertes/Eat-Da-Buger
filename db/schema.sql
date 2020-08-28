e1229c9e6e700ebc6bc30cb07361a82c672c9fca
Cristhal Fuertes <cristhal.fuertes@gmail.com>
2020-08-28 19:23:38 -0400

    fixed spelling errors in sql files

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,9 @@
-DROP DATABASE IF EXISTS burger_db;
+DROP DATABASE IF EXISTS burgers_db;
 
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
-CREATE TABLE burgers (
+CREATE TABLE burgers(
 id int AUTO_INCREMENT NOT NULL PRIMARY KEY,
 burger_name VARCHAR (50) NOT NULL,
 devoured BOOLEAN DEFAULT false,
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO bugers (burger_name, devoured) VALUES ('Cheeseburger', FALSE);
-INSERT INTO bugers (burger_name, devoured) VALUES ('Veggie Burger', FALSE);
-INSERT INTO bugers (burger_name, devoured) VALUES ('Beyond Burger', TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Beyond Burger', TRUE);
