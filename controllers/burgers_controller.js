7200d6158f4a34cebdbfb8687f970c9b7beea1c7
Cristhal Fuertes <cristhal.fuertes@gmail.com>
2020-08-29 08:21:02 -0400

    forgot to add express in bugers_controller.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,6 +1,5 @@
-// * Express
 var express = require("express");
-// * `burger.js`
+var router = express.Router();
 
 var burger = require("../models/burger.js");
 // 4. Create the `router` for the app, and export the `router` at the end of your file.
