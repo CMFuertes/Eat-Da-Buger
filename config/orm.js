e15ed85c899b3584a2b20b6e16f04d513fcdd118
Cristhal Fuertes <cristhal.fuertes@gmail.com>
2020-08-29 06:45:29 -0400

    started adding functions to orm

---
+++ config/orm.js
@@ -0,0 +1,16 @@
+var connection = require("./connection.js");
+
+var orm = {
+
+    selectAll: function () {
+        var queryString = "";
+    },
+    insertOne: function ( ) {
+
+    },
+    updateOne: function ( ) {
+
+    },
+};
+
+module.exports = orm;
