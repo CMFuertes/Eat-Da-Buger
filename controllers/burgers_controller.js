78acc9c57d9342eeca8f657d17db5e439e2719dd
Cristhal Fuertes <cristhal.fuertes@gmail.com>
2020-08-29 07:33:56 -0400

    imported express and burger.js into burgers_controller.js

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,6 @@
+// * Express
+var express = require("express");
+// * `burger.js`
+
+var burger = require("../models/burger.js");
+// 4. Create the `router` for the app, and export the `router` at the end of your file.
