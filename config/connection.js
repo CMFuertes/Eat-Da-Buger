404fa27850f750f46c5a4078e378aec1b84e1a6b
Cristhal Fuertes <cristhal.fuertes@gmail.com>
2020-08-31 16:32:37 -0400

    heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 var mysql = require("mysql");
+var connection ;
+if (process.env.JAWSDBS_URL){
+connection = mysql.createConnection(process.env.JAWSDBS_URL)
+}
 
-var connection = mysql.createConnection({
+else 
+{connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "Belle!123",
   database: "burgers_db"
-});
+});}
+
 
 connection.connect(function(err) {
     if (err) {
