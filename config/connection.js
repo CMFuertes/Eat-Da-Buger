f7c7684d84a3ba15f92c78e8e67b4cba9741a0ce
Cristhal Fuertes <cristhal.fuertes@gmail.com>
2020-08-31 16:44:34 -0400

    error fixed

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,10 @@
 var mysql = require("mysql");
 var connection ;
-if (process.env.JAWSDBS_URL){
-connection = mysql.createConnection(process.env.JAWSDBS_URL)
-}
-
-else 
-{connection = mysql.createConnection({
+if (process.env.JAWSDB_URL){
+connection = mysql.createConnection(process.env.JAWSDB_URL)
+}else 
+{
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
